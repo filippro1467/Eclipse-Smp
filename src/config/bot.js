90e0df9eb2822b4d42528d231be885aaac2d806c
filippro1467 <ftraczykowski@gmail.com>
2026-06-01 20:23:48 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Eclipse Smp",
+        name: "Revive Smp",
         // Activity type number (0 = Playing).
         type: 0, 
       },
